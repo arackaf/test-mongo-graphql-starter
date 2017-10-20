7ff6689bac79381fa85c666e435a5f64b3d8f2e8
Adam Rackis <adamrackis@hotmail.com>
2017-10-20 08:45:56 -0500

    initial repro

---
+++ runIt.js
@@ -0,0 +1,6 @@
+import { createGraphqlSchema } from "mongo-graphql-starter";
+import projectSetup from "./projectSetupA";
+
+import path from "path";
+
+createGraphqlSchema(projectSetup, path.resolve("./testProject1"));
